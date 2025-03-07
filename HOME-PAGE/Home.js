cb26adfff234a2e16de45b93fa352b25f13214fa
Dozie <bonannadozie@gmail.com>
2025-03-07 13:29:36 +0100

    resellers project v1

---
+++ HOME-PAGE/Home.js
@@ -0,0 +1,14 @@
+// this js code below is to override autoplay policy of the browser there by giving external class to my video
+
+document.addEventListener("DOMContentLoaded",function(){
+  document.getElementById("slide1").classList.add("slide1");
+}); 
+
+document.addEventListener("DOMContentLoaded",function(){
+  document.getElementById("shuffle-box").classList.add("shuffle-box");
+}); 
+
+
+onclick= {
+  
+}
